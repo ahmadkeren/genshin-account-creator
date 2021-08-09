aad4ff2d5c9eb4d872e708efc452e8625ad11a91
sadru <thesadru@gmail.com>
2021-08-09 22:22:47 +0200

    Reworked code to actually generate hoyolabs account

--- README.md
+++ README.md
@@ -3,12 +3,17 @@ A batch genshin account creator using selenium
 
 All captchas must be solved by the user, there is a 60 second timeout if you take too long.
 
+# usage
+Run `python main.py` and wait for the accounts to be generated.
+Every time a captcha pops out solve it.
+
 # how it works
 1. Create a [temp-mail](https://temp-mail.org/) address
 2. Register through the [official website](https://account.mihoyo.com/#/register/email)
 3. Wait for the confirmation code to arrive at your address
 4. Verify your account
-5. Login through the [official website](https://account.mihoyo.com/#/login)
-6. Fetch cookies using the mihoyo api.
-7. Store the email, password and cookies in accounts
-8. Logout
+5. Login on [hoylabs](https://www.hoyolab.com/genshin/)
+6. Create a new hoyolabs account using the email as the username
+7. Fetch cookies by going to mihoyo's api subdomain.
+8. Store the email, password, username and cookies.
+9. Logout
--- main.py
+++ main.py
@@ -1,60 +1,94 @@
+import argparse
 import json
+import random
+import string
+import time
+import random
+import argparse
 import traceback
+from typing import Any, Collection
 
-import requests
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-PASSWORD = 'wordpass12'
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--output', metavar="PATH", default="accounts.json", help="The output json file")
+parser.add_argument('--password', help="The pasword for all accounts, if not provided will be made randomly")
+parser.add_argument('--cookies', action='store_true', help="Only outputs a list of cookies instead of entire accounts")
+args = parser.parse_args()
 
 driver = webdriver.Firefox()
 
-def switch(window: int) -> None:
+def _generate_password() -> str:
+    """Generate a new password that's at least medium for mihoyo"""
+    while True:
+        password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+        if any(i.isdigit() for i in password) and not all(i.isdigit() for i in password):
+            return password
+
+def switch_window(window: int) -> None:
     """Helper function to switch to a tab"""
     driver.switch_to.window(driver.window_handles[window])
 
-# create two extra tabs
-driver.execute_script(f"window.open('about:blank');")
-driver.execute_script(f"window.open('about:blank');")
-
-# open each tab on its page
-urls = [
-    'https://account.mihoyo.com/#/register/email',
-    'https://account.mihoyo.com/#/login',
-    'https://temp-mail.org/en/'
-]
-for handle,url in zip(driver.window_handles, urls):
-    driver.switch_to.window(handle)
-    driver.get(url)
+def init_windows(start: int = 0) -> None:
+    """Initialize windows with required sites"""
+    switch_window(start)
+    driver.get('https://account.mihoyo.com/#/register/email')
+    driver.execute_script(f"window.open('about:blank');")
+    switch_window(start + 1)
+    driver.execute_script(f"window.open('about:blank');")
+    driver.get('https://www.hoyolab.com/genshin/')
+    switch_window(start + 2)
+    driver.get('https://temp-mail.org/en/')
 
 def get_email_address() -> str:
-    """Gets the current mail address"""
-    switch(2)
+    """Gets the current mail address from temp-mail
+    
+    Will wait for the address to be generated if not avalible
+    """
+    switch_window(2)
     print('getting email adress')
     while True:
-        mail = driver.execute_script("return document.getElementById('mail').value")
+        try:
+            mail = driver.find_element_by_id('mail').get_attribute('value')
+        except:
+            driver.refresh() # there's a ddos protection that sometimes locks you out
+            mail = ''
         if mail and '@' in mail:
             return mail
         
-        driver.implicitly_wait(.1)
+        time.sleep(.1)
 
-def wait_for_confirmation() -> str:
+def wait_for_confirmation(delete_after: bool = True) -> str:
     """Waits for a confirmation email and returns its code"""
-    switch(2)
-    print('waiting for confirmation email')
-    element = WebDriverWait(driver, 30).until(
+    switch_window(2)
+    print('waiting for confirmation email (may take up to a minute!)')
+    element = WebDriverWait(driver, 60).until(
         EC.presence_of_element_located(('xpath', '//*[@id="tm-body"]/main/div[1]/div/div[2]/div[2]/div/div[1]/div/div[4]/ul/li[2]/div[2]/span/a'))
     )
     text = element.get_attribute('textContent')
-    driver.find_element_by_id('click-to-delete').click() # email is no longer needed so we can delete it
+    
+    if delete_after:
+        # this causes tempmail to trigger its antibot protection
+        # we either need to solve that or reload sometime later when we have time
+        # there's however not a clean way to transition from captcha to reloading 
+        # so we sacrifice like 2s to this
+        driver.find_element_by_id('click-to-delete').click()
+    
     return text.strip().split(' ')[0]
 
 
-def fill_out_register_form(email: str, password: str) -> None:
-    """Fills out the registration form"""
-    switch(0)
+def register(email: str, password: str) -> None:
+    """Fills out the registration form 
+    
+    In the rare case that the user is not logged in after registering
+    they will be redirected to the login page.
+    """
+    switch_window(0)
     driver.get('https://account.mihoyo.com/#/register/email')
+    # wait until reloaded
+    WebDriverWait(driver, 2).until(EC.text_to_be_present_in_element(('tag name', 'body'), "Register"))
     # fill out form
     print(f'filling out registration form for {email}')
     driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/form/div[1]/div/input').send_keys(email)
@@ -69,63 +103,124 @@ def fill_out_register_form(email: str, password: str) -> None:
         EC.text_to_be_present_in_element(('xpath', '//*[@id="root"]/div[1]/div[2]/form/div[2]/div/div'), 'Sent')
     )
     code = wait_for_confirmation()
-    switch(0)
+    switch_window(0)
     driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/form/div[2]/div/input').send_keys(code)
-    for _ in range(5): # just click it a few times to be sure, idk wtf is going on with mihoyo
-        driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/form/div[6]/button').click()
-        driver.implicitly_wait(.2)
+    try:
+        # sometimes the button just doesn't work, so do it 3 extra times than needed
+        for _ in range(5):
+            driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/form/div[6]/button').click()
+            time.sleep(.2)
+    except:
+        pass
+    
     driver.refresh() # clear out the fields
     print(f'registered email: {email} password: {password} code: {code}')
+    
+    if 'Register' in driver.find_element_by_tag_name('body').text: # still not logged in
+        login(email, password)
 
-def register_account() -> tuple[str, str]:
-    """Registers a new account, returns its email and password"""
-    email = get_email_address()
-    fill_out_register_form(email, PASSWORD)
-    return (email, PASSWORD)
-
+def create_hoyolab_account(email: str, password: str) -> str:
+    """Creates a hoyolab account on the hoyolab website, returns the username"""
+    print('creating a hoyolab account')
+    switch_window(1)
+    # wait until reloaded
+    element = WebDriverWait(driver, 5).until(EC.presence_of_element_located(('xpath', '//*[@id="__layout"]/div/div[1]/div/div[2]/div[1]/div/div/img')))
+    element.click()
+    
+    driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div/div[2]/div/form/div[1]/div/input').send_keys(email)
+    driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div/div[2]/div/form/div[2]/div/input').send_keys(password)
+    driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div/div[2]/div/form/button').click()
+    
+    print('waiting for captcha to be solved')
+    element = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located(('xpath', '//*[@id="__layout"]/div/div[4]/div/div[2]/div[2]/div[1]/div[1]/input'))
+    )
+    username = email[:20]
+    element.send_keys(username)
+    driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div/div[2]/div[2]/div[2]/div/div/i').click()
+    driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div/div[2]/div[2]/div[3]/button').click()
+    # there used to be a bug where you would get a tos agreement form twice
+    
+    driver.refresh() # clear out the fields
+    return username
 
 def login(email: str, password: str) -> None:
-    """Logs into a mihoyo account, returns the cookies"""
+    """Logs into a mihoyo account using the login page
+    
+    Uses the same window as the registration.
+    """
     if driver.get_cookie('login_ticket') is not None:
         return # already logged in
-    switch(1)
+    
     print('logging in')
+    switch_window(0)
+    driver.get('https://account.mihoyo.com/#/login')
+    
     driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/form/div[1]/div/input').send_keys(email)
     driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/form/div[2]/div/input').send_keys(password)
     driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/form/div[3]/button').click()
+    
     print('waiting for captcha to be solved')
     WebDriverWait(driver, 60).until(
         EC.text_to_be_present_in_element(('tag name', 'body'), 'Account Information')
     )
+    driver.get('https://account.mihoyo.com/#/register/email')
 
-def logout():
-    """Logs out of a mihoyo acccount"""
-    print('logging out of account')
-    switch(1)
-    driver.delete_all_cookies()
-    driver.get('https://account.mihoyo.com/#/login')
+def create_account(password: str = None) -> tuple[str, str]:
+    """Creates a new mihoyo account
+    
+    This is done by creating an email address, registering the user and possibly logging in.
+    """
+    password = password or _generate_password()
+    
+    email = get_email_address()
+    register(email, password)
+    return email, password
 
-def get_cookies() -> dict[str, str]:
-    """Returns a list of old-style """
+def get_cookies(url: str = "https://api-os-takumi.mihoyo.com/", allowed: Collection[str] = {'ltoken', 'ltuid'}) -> dict[str, str]:
+    """Returns a list of cookies from a url"""
     print('fetching login cookies')
-    login_ticket = driver.get_cookie('login_ticket')['value'] # type: ignore
-    r = requests.get(f"https://webapi-os.account.mihoyo.com/Api/cookie_accountinfo_by_loginticket?login_ticket={login_ticket}")
-    return dict(r.cookies)
+    # new tab
+    switch_window(-1)
+    driver.execute_script(f"window.open('about:blank');")
+    switch_window(-1)
 
+    driver.get(url)
+    cookies = {c['name']: c['value'] for c in driver.get_cookies() if c['name'] in allowed}
+    driver.delete_all_cookies()
+    driver.close()
+    
+    return cookies
 
-def run() -> list[dict]:
+def clear_cookies() -> None:
+    """Deletes cookies from all windows"""
+    for handle in driver.window_handles:
+        driver.switch_to.window(handle)
+        driver.delete_all_cookies()
+
+def run() -> list[dict[str, Any]]:
+    """Creates accounts until an error is encountered"""
+    init_windows()
     accounts = []
     try:
         while True:
             print('='*50)
-            email, password = register_account()
-            login(email, password)
-            accounts.append({
-                'email': email,
-                'password': password,
-                'cookies': get_cookies()
-            })
-            logout()
+            email, password = create_account(args.password)
+            username = create_hoyolab_account(email, password)
+            cookies = get_cookies()
+            
+            if args.cookies:
+                accounts.append(cookies)
+            else:
+                accounts.append({
+                    'email': email,
+                    'password': password,
+                    'username': username,
+                    'account_id': int(cookies['ltuid']),
+                    'cookies': cookies
+                })
+            
+            clear_cookies()
     except Exception as e:
         traceback.print_exc()
     finally:
@@ -133,9 +228,15 @@ def run() -> list[dict]:
     
 if __name__ == '__main__':
     accounts = run()
-    with open('accounts.json', 'r') as file:
-        data = json.load(file)
-    data.extend(accounts)
-    with open('accounts.json', 'w') as file:
-        json.dump(data, file, indent=4)
+    try:
+        with open(args.output) as file:
+            data = json.load(file)
+            accounts = data + accounts
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        print("error reading file: ", e)
+    
+    with open(args.output, 'w') as file:
+        json.dump(accounts, file, indent=4)
     
